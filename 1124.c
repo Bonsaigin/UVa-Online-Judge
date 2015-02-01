449b119cfcfc70ddde7767a456b919dafbe4573d
Ian Gibson <itgi222@cs.uky.edu>
2015-02-01 14:01:15 -0500

    Celebrity Jeopardy

---
+++ 1124.c
@@ -0,0 +1,14 @@
+/* Celebrity Jeopardy 
+ * http://uva.onlinejudge.org/external/11/1124.html
+ * I/O copy 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+  char c;
+  while (( c = getchar() ) != EOF ) {
+    putchar(c);
+  }
+  return 0;
+}
